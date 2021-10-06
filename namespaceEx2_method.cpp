abfe8833fd4c697869fec7c8d6978c12938cf135
moses mccabe <moses.peace.mccabe@gmail.com>
2021-10-05 17:26:39 -0700

    create a namespace C++

---
+++ namespaceEx2_method.cpp
@@ -0,0 +1,35 @@
+/*  Moses Peace Mccabe
+ 
+ Understanding C++ namespace
+ */
+#include<iostream>
+using namespace std;
+
+namespace boss
+{
+    int val = 5;
+    void display()
+    {
+        cout << "\nWelcome Msg for Namespace\n";
+    }
+}
+void display()
+{
+    cout << "Welcome Msg from Global Method\n";
+}
+
+// Global Variable
+int val = 10;
+
+int main()
+{
+    int val = 100;                                 // local variable
+    cout << "\nThe value of Val is: " << val       // local variable
+         << "\nThe value of Global Variable is: " << ::val  // :: scope resolution operator
+         << "\nThe Name Space variable is: " << boss::val << endl; // namespace variable
+    boss::display();
+    display();
+    
+    
+    return 0;
+}
